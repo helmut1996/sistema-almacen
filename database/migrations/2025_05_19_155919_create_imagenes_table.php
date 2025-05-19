1b25bcd278b3b12babfd3df9df881b2afe067881
helmut1996 <josubrenes0@gmail.com>
2025-05-19 10:15:27 -0600

    Creacion del proyecto

---
+++ database/migrations/2025_05_19_155919_create_imagenes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('imagenes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_producto')->constrained('producto')->cascadeOnDelete();
+            $table->string('nombre',500);
+            $table->string('ruta',500);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('imagenes');
+    }
+};
